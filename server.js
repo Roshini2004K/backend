9cbdabd653f5a5d7257e0e119d26a4d5fc18fa45
Roshini12004K <roshini1924@gmail.com>
2026-02-27 16:48:41 +0530

    add CORS headers

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+})
+
 server.use(middlewares)
 server.use(router)
 server.listen(10000, () => {
-  console.log('JSON Server is running on port 10000')
+  console.log('JSON Server is running')
 })
